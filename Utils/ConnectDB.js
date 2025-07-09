cb3df230c5ebd501cb1f24367be84030965caab8
Biswajit Sharma <biswajit@Biswajit.>
2025-07-09 08:46:22 +0000

    all

--- Utils/ConnectDB.js
+++ Utils/ConnectDB.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config();
 //connection to db ..
-const Connection = () => mongoose.connect('mongodb+srv://biswajitshrm6:7DL0Lz8dxicjlXQJ@users.mt5yvfh.mongodb.net/FlashFire')
+const Connection = () => mongoose.connect(process.env.MONGODB_URI)
                     .then(()=>console.log("Database connected succesfully..!"))
                     .catch((e)=>console.log('Problem while connecting to db', e));
 
