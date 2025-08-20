651d165353693e367193ca75ad6cca095bbd7f20
Biswajit Sharma <biswajit@Biswajit.>
2025-08-20 20:56:13 +0000

    rf

--- Schema_Models/JobModel.js
+++ Schema_Models/JobModel.js
@@ -24,7 +24,7 @@ export const JobSchema = new mongoose.Schema({
   currentStatus : {
     type : String,
     required : true,
-    default : 'Added'
+    default : 'saved'
   },
   jobDescription: {
     type: String,
